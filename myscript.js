ea9682e79894a0859cb86e36b59ba42240b7e707
AINDRILAROY <104571646+AINDRILAROY@users.noreply.github.com>
2022-06-06 16:36:22 +0530

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+      items: 8,
+      pagination:false,
+      itemsDesktop: [1199, 5],
+      itemsDesktopSmall: [979, 5],
+      navigation: true,
+      navigationText: [
+        "<i class='fa fa-chevron-left'></i>",
+        "<i class='fa fa-chevron-right'></i>"
+      ]
+    });
+  });
